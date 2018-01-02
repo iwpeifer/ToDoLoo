d249c582fb26233fdd433daffeb76687aefa85e4
Isaac Peifer <iwpeifer@gmail.com>
2018-01-02 16:48:38 -0700

    tasks can now be marked as complete or uncomplete

---
+++ database/migrations/2018_01_02_231007_add_completed_to_tasks.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCompletedToTasks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::table('tasks', function($table)
+      {
+        $table->boolean('completed')->default(false);
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
